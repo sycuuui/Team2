d47408117b18e65667300bf382cff66a670d44f9
yoons02 <kys5037k@naver.com>
2023-02-22 21:16:03 +0900

    문제 모델

--- together/myapp/models.py
+++ together/myapp/models.py
@@ -14,13 +14,8 @@ class Question(models.Model):
     
     img = models.ImageField(max_length=500, null=True, blank=True)
 
-    def __str__(self):
-        return str(self.id)+"번 문제"
     
-class User(models.Model):
-    univ = models.CharField(max_length=30)
+class User(models.Model): 
     answer = models.CharField(default="", max_length=10, null=False, blank=False)
     score = models.IntegerField(default=0, null=True, blank=True)
 
-    def __str__(self):
-        return self.univ
--- together/myapp/views.py
+++ together/myapp/views.py
@@ -21,7 +21,26 @@ def showuniv_3(request):
     return render(request, 'myapp/univ_3.html')
 
 def showprob(request):
-    return render(request, 'myapp/prob.html')
+    quizes = Question.objects.all()
+    return render(request, 'myapp/prob.html', {'quizes' :quizes})
 
 def showparticipate(request):
     return render(request, 'myapp/participate.html')
+
+# def quiz(request, pk):
+#     user = get_list_or_404(User, pk=pk)
+#     aans = get_list_or_404(Answer)
+
+#     num = 1
+#     if request.POST:
+#         num = int(request.POST['quiz_id'])+1
+#         user.answer = user.answer + request.POST['answer']
+#         if request.POST['answer'] == aans.ans[num-2]:
+#             user.score += 1
+#             user.save()
+
+#         if num > 4:
+#             return redirect('result', pk)
+        
+#     prob = get_list_or_404(Question, id = num)
+#     return render(request, "prob.html", {'prob':prob})
