248f75ee4ee1540c09e3f7590cf5b5256b918283
yoons02 <kys5037k@naver.com>
2023-02-22 22:30:02 +0900

    학교 모델

--- together/myapp/views.py
+++ together/myapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_list_or_404,redirect
-from .models import Answer, Question, User
+from .models import Answer, Question, User, Univ
+import random
 
 # Create your views here.
 def showlanding(request):
@@ -11,17 +12,14 @@ def showintro(request):
 def showintro2(request):
     return render(request, 'myapp/intro2.html')
 
-def showuniv_1(request):
-    return render(request, 'myapp/univ_1.html')
-
-def showuniv_2(request):
-    return render(request, 'myapp/univ_2.html')
-
-def showuniv_3(request):
-    return render(request, 'myapp/univ_3.html')
+def showuniv(request):
+    unives = Univ.objects.all()
+    return render(request, 'myapp/univ_1.html', {'unives' : unives})
 
 def showprob(request):
-    quizes = Question.objects.all()
+    # quizes = Question.objects.all()
+    quizes = list(Question.objects.all())
+    quizes = random.sample(quizes, 10)
     return render(request, 'myapp/prob.html', {'quizes' :quizes})
 
 def showparticipate(request):
