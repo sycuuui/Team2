cd3cb9f68502abf1d7c8ec0b2f03d25855182099
yoons02 <kys5037k@naver.com>
2023-02-23 22:07:37 +0900

    url static

--- together/together/urls.py
+++ together/together/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from myapp import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,3 +28,5 @@ urlpatterns = [
     path('prob/', views.showprob, name="showprob"),
     path('participate/', views.showparticipate, name="showparticipate")
 ]
+
+urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
